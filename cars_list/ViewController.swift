2d68dd6edb429b366ba13051664e6c3fb7379286
damir <damirkaniev905@gmail.com>
2022-03-31 18:53:52 +0600

    Initial Commit

---
+++ cars_list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cars_list
+//
+//  Created by admin on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
